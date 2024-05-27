8e8e2aedec84e3edfabc19da4ffdf10c7d2f6f45
Stephan-Marie Rubayiza <rubayizastefan@gmail.com>
2024-05-27 16:26:09 +0000

    first commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { bootstrapCameraKit } from '@snap/camera-kit';
+
+(async function () {
+  const cameraKit = await bootstrapCameraKit({ 
+    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzE2ODE2ODQ2LCJzdWIiOiIzZmUzYzEyMy1lODdlLTQzNjEtODYwMi1jODkxMzNlYzMyOWN-U1RBR0lOR34wOThhY2U3MS01OWQ0LTQ2NzUtYjk0MS04YjUyMmRiMjJiZGEifQ.Vndej82iEI6kjWKSS9dG1gnoRbjVzBYq32vwq_NLV8o' });
+
+const liveRenderTarget = document.getElementById('canvas') as HTMLCanvasElement;
+
+const session = await cameraKit.createSession({ liveRenderTarget });
+
+const mediaStream = await navigator.mediaDevices.getUserMedia({
+  video: true,
+});
+
+await session.setSource(mediaStream);
+await session.play();
+
+const lens = await cameraKit.lensRepository.loadLens(
+  '50507980875',
+  '7fa3fa7c-e626-4539-b9db-73cdb0b0b2ce'
+);
+
+await session.applyLens(lens);
+
+})();
