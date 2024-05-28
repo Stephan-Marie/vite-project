3982e06423b9b3c551783760fb6897adfdc69c81
Stephan-Marie Rubayiza <rubayizastefan@gmail.com>
2024-05-28 10:37:53 +0000

    screen fix

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, './index.html'),
         docs: resolve(__dirname, 'docs/index.html'),
       },
     },
