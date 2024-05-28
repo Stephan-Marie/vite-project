00e1ef5aa4ccf3c29e1b466f32485b1bc14ce0d5
Stephan-Marie Rubayiza <rubayizastefan@gmail.com>
2024-05-28 11:23:09 +0000

    screenfix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { bootstrapCameraKit, createMediaStreamSource, Transform2D, } from '@snap
   const cameraKit = await bootstrapCameraKit({ 
     apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzE2ODE2ODQ2LCJzdWIiOiIzZmUzYzEyMy1lODdlLTQzNjEtODYwMi1jODkxMzNlYzMyOWN-U1RBR0lOR34wOThhY2U3MS01OWQ0LTQ2NzUtYjk0MS04YjUyMmRiMjJiZGEifQ.Vndej82iEI6kjWKSS9dG1gnoRbjVzBYq32vwq_NLV8o' });
 
-const liveRenderTarget = document.getElementById('canvas') as HTMLCanvasElement
+const liveRenderTarget = document.getElementById('canvas') as HTMLCanvasElement;
 
 const session = await cameraKit.createSession({ liveRenderTarget })
 
