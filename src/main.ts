f1cdf419e6c08533d4f37272f5f8b2ca1517615a
Stephan-Marie Rubayiza <rubayizastefan@gmail.com>
2024-05-28 10:02:30 +0000

    screen fix

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,16 @@ import { bootstrapCameraKit, createMediaStreamSource, Transform2D, } from '@snap
   const cameraKit = await bootstrapCameraKit({ 
     apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzE2ODE2ODQ2LCJzdWIiOiIzZmUzYzEyMy1lODdlLTQzNjEtODYwMi1jODkxMzNlYzMyOWN-U1RBR0lOR34wOThhY2U3MS01OWQ0LTQ2NzUtYjk0MS04YjUyMmRiMjJiZGEifQ.Vndej82iEI6kjWKSS9dG1gnoRbjVzBYq32vwq_NLV8o' });
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const liveRenderTarget = document.getElementById('canvas') as HTMLCanvasElement
 
-const session = await cameraKit.createSession({ liveRenderTarget:canvas });
+const session = await cameraKit.createSession({ liveRenderTarget })
 
 const {lenses} = await cameraKit.lensRepository.loadLensGroups(['7fa3fa7c-e626-4539-b9db-73cdb0b0b2ce'])
 
 await session.applyLens(lenses[3])
 
 const mediaStream = await navigator.mediaDevices.getUserMedia({
-  video: true
+  video: {width: 320, height: 180}
 });
 
 const source = createMediaStreamSource(mediaStream, {
@@ -23,9 +23,9 @@ const source = createMediaStreamSource(mediaStream, {
 
 await session.setSource(source);
 
-session.play();
+//source.setRenderSize(window.innerWidth, window.innerHeight)
 
-source.setRenderSize(1080, 1920)
+session.play();
 
 //const lens = await cameraKit.lensRepository.loadLens(
 //  '50507980875',
