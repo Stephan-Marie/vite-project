e84f7be4941114aac4b110c6800b8fd83607b6e5
Stephan-Marie Rubayiza <rubayizastefan@gmail.com>
2024-05-28 10:22:46 +0000

    screen fix

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ const {lenses} = await cameraKit.lensRepository.loadLensGroups(['7fa3fa7c-e626-4
 await session.applyLens(lenses[3])
 
 const mediaStream = await navigator.mediaDevices.getUserMedia({
-  video: true
+  video: {aspectRatio: 1080/1920}
 });
 
 const source = createMediaStreamSource(mediaStream, {
